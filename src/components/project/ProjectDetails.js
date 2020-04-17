78c90d2421962a4b1204a298bb67c36bfbbcf11f
Hritik Harbhla <harbhlahritik@gmail.com>
2020-04-18 03:42:38 +0530

    Implemented Firebase Authentication along with Login and Logout in Components

--- src/components/project/ProjectDetails.js
+++ src/components/project/ProjectDetails.js
@@ -1,23 +1,49 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 const ProjectDetails = (props) => {
-    const id = props.match.params.id;
-    return (
-        <div>
-            <div className="container section project-details">
-                <div className="card z-depth-0">
-                    <div className="card-content">
-                        <span className="card-title">Project Title - {id}</span>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique consectetur qui porro reiciendis corrupti enim velit reprehenderit commodi incidunt fuga, molestiae temporibus iusto! Quasi natus ullam repellendus modi laudantium. Molestiae.</p>
-                    </div>
-                    <div className="card-action grey lighten-4 grey-text">
-                        <div>Posted by Hritik Harbhla</div>
-                        <div>2nd April, 5pm</div>
+    const { project } = props;
+    if (project) {
+        return (
+            <div>
+                <div className="container section project-details">
+                    <div className="card z-depth-0">
+                        <div className="card-content">
+                            <span className="card-title">{ project.title }</span>
+                            <p>{ project.content }</p>
+                        </div>
+                        <div className="card-action grey lighten-4 grey-text">
+                            <div>Posted by { project.authorFirstName } { project.authorLastName }</div>
+                            <div>2nd April, 5pm</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="container center">
+                <p>Loading project...</p>
+            </div>
+        )
+    }
 }
 
-export default ProjectDetails
+const mapStatetoProps = (state, ownProps) => {
+    // console.log(state)
+    const id = ownProps.match.params.id;
+    const projects = state.firestore.data.projects;
+    const project = projects ? projects[id] : null
+    return {
+        project: project
+    }
+}
+
+export default compose(
+    connect(mapStatetoProps),
+    firestoreConnect([
+        { collection: 'projects' }
+    ])
+)(ProjectDetails)
